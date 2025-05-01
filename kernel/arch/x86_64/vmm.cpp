c122c81fd269690611de7783c227c235ba135d1b
Kross1de <awesome.hubygu@yandex.ru>
2025-05-01 18:28:25 +0600

    Delete kernel directory

--- kernel/arch/x86_64/vmm.cpp
+++
@@ -1,9 +0,0 @@
-#include "include/vmm.hpp"
-
-extern "C"
-{
-    alignas(vmm::PAGE_SIZE) uint64_t pml4[512];
-    alignas(vmm::PAGE_SIZE) uint64_t pdp[512];
-    alignas(vmm::PAGE_SIZE) uint64_t pd[512];
-    alignas(vmm::PAGE_SIZE) uint64_t first_pt[512];
-}
