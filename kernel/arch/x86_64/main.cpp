e436381183b0ad68a0fb90cc81beef40e83808f5
Kross1de <awesome.hubygu@yandex.ru>
2025-05-01 15:25:26 +0600

    printf tests

--- kernel/arch/x86_64/main.cpp
+++ kernel/arch/x86_64/main.cpp
@@ -1,30 +1,19 @@
-#include "include/serial.hpp"
 #include "include/string.hpp"
+#include "include/printf.hpp"
 
 extern "C" void kmain(void *mboot_info)
 {
-    serial::init();
-    serial::write_string("Kernel initialized!\n");
+
+    kprintf::printf("kernel loaded\n");
 
     // test
-    char buffer[32];
     const char *test = "Hello, world!";
-    kstring::strcpy(buffer, test);
-    serial::write_string("copied string: ");
-    serial::write_string(buffer);
-    serial::write_char('\n');
-
-    serial::write_string("string length: ");
-    char len_str[16];
+    kprintf::printf("copied string: %s\n", test);
 
     size_t len = kstring::strlen(test);
-    len_str[0] = '0' + (len / 10);
-    len_str[1] = '0' + (len % 10);
-    serial::write_string(len_str);
-    serial::write_char('\n');
+    kprintf::printf("string length: %d\n", len);
 
-    while (true)
-    {
+    while (true) {
         asm volatile("hlt");
     }
 }
